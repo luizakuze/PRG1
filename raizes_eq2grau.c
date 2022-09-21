971f629894759c5c50f3a8377c4509b88372bf12
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2022-09-21 13:41:51 -0300

    Update raizes_eq2grau.c

--- raizes_eq2grau.c
+++ raizes_eq2grau.c
@@ -12,8 +12,8 @@ int main()
 	delta = b * b - 4 * a * c;	
 
 	if (delta > 0) {
-		x1 = ((-b + sqrtf(delta)) / ( 2 * a );
-		x2 = ((-b - sqrtf(delta)) / ( 2 * a );
+		x1 = (-b + sqrtf(delta)) / ( 2 * a );
+		x2 = (-b - sqrtf(delta)) / ( 2 * a );
 		printf("As raízes da equação são: %f e %f.\n", x1, x2);
 	} else if (delta == 0 ) {
 		x1 = -b / ( 2 * a );
@@ -21,8 +21,9 @@ int main()
 	} else {
 		parte_real = -b / ( 2 * a );
 		parte_imag = sqrtf(-delta) / ( 2 * a );
-		printf("Raízes complexas.\n");
-		printf("Raíz1: %f +/- j %f\n", parte_real, parte_imag);
+		printf("2 Raízes Complexas.\n");
+		printf("Raíz 1: %.3f + j %.3f\n", parte_real, parte_imag);
+		printf("Raíz 2: %.3f - j %.3f\n", parte_real, parte_imag);
 	}
 	return 0;
 }
