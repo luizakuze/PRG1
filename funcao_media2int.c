e8a0a5fb6403c31e1b92e77b24847c88662f6187
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2022-09-28 14:10:32 -0300

    Update and rename TESTEEE to funcao_media2int.c

--- funcao_media2int.c
+++ funcao_media2int.c
@@ -6,17 +6,19 @@ float calculo_media (int num1, int num2)
 {
 	float media;
 
-	media = num1 + num2 / 2.0;
+	media = (num1 + num2) / 2.0;
 	return media;
 }
 int main()
 {
-	int num1, num2, media;
+	int num1, num2;
+	float media;
 	
-	printf("Entre com o valor de dois números:\n")
+	printf("Entre com os valores de 2 números inteiros: \n");
 	scanf("%d %d", &num1, &num2);
  
-	printf("A média entre %d e %d é: %f\n", num1, num2, media);
+	media = calculo_media (num1, num2);
+	printf("A média entre %d e %d é: %.2f\n", num1, num2, media);
 
 	/* teste da função calculo_media
 	media = calculo_media(1, 2);
