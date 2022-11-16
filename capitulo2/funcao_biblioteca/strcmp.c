f5654662a53abecec1d0521976d14aa0f670a8ab
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2022-11-16 08:29:43 -0300

    Update strcmp.c

--- capitulo2/funcao_biblioteca/strcmp.c
+++ capitulo2/funcao_biblioteca/strcmp.c
@@ -1,22 +1,21 @@
-/* O programa lê 2 cadeias e imprime uma mensagem caso elas sejam iguais */
+// O programa compara 2 strings
 
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
-    char x[6], y[6];
-    int comparacao;
+    char x[50], y[50];
 
     printf("Entre com a 1 string:\n");
     scanf("%s", x);
     printf("Entre com a 2 string:\n");
     scanf("%s", y);
 
-    comparacao = strcmp (x, y);
-    if (comparacao == 0) 
-        printf("As strings são iguais\n");
+    if (strcmp (x, y) == 0) 
+        printf("As strings são iguais.\n");
     else
-        printf("Fim do programa\n");
+        printf("As strings são diferentes.\n");
+
     return 0;
 }
