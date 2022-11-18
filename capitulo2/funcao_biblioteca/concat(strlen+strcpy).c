51f429ceaf79fb998da526fcb467ca5bd14ecb4c
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2022-11-17 22:19:56 -0300

    Create concat(strlen+strcpy).c

---
+++ capitulo2/funcao_biblioteca/concat(strlen+strcpy).c
@@ -0,0 +1,17 @@
+/* O programa aplica a função de concatenação de 2 strings sem utilizar a função "strcpy", 
+utilizando a "strlen" e "strcpy" */
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char x[10] = "ifsc", y[10] = "-sj";
+    int termina_string;
+
+    strcpy(&x[strlen(x)], y);
+
+    printf("%s\n", x); 
+
+    return 0;
+}
