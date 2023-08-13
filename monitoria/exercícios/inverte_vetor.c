2a14bfa4d7e45d07f0e9439589b25b8d12b51c9e
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-08-13 20:46:48 -0300

    Create inverte_vetor.c

---
+++ monitoria/exercícios/inverte_vetor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// função para inverter a ordem do vetor
+void inverte (int vet[]) {
+    int aux[10];
+    for (int i = 0; i < 10; i++) {
+            aux[i] = vet[9-i];
+    }
+
+    for (int i = 0; i < 10; i++) {
+        vet[i] = aux[i];
+    }
+}
+
+// função para imprimir o vetor
+void imprime (int vet[]) {
+    for (int i = 0; i < 10; i++) {
+        printf("vet[%d] = %d\n", i, vet[i]);
+    }
+}
+
+int main() {
+    int vet[10] = {0,1,2,3,4,5,6,7,8,9};
+
+    printf("Antes da inversão:\n");
+    imprime(vet);
+
+    printf("\nApós inversão:\n");
+    inverte(vet);
+    imprime(vet);
+
+    return 0;
+}
