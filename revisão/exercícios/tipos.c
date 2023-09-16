0bdca494b017e6ba91aa61b5976d7067c8c7f881
Luiza Kuze <111708035+luizakuze@users.noreply.github.com>
2023-09-16 16:09:00 -0300

    Create tipos.c

---
+++ revisão/exercícios/tipos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    // Revisão de como imprimir os tipos
+    int A = 1;
+    float B = 2.4; //2.4f
+    char C = 'L';
+    char D[10] = "luiza";
+    double E = 3.6;
+
+    printf("%d %f %c %s %lf\n", A, B, C, D, E);
+
+    return 0;
+}
